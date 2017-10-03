1696010018e88732145f4273df00b0a253cb9798
Jeremy Pichon <jeremy.pichon@fr.ibm.com>
2017-10-03 14:18:56 +0200

    Fixing unzipping ODM resources in build.sh.
    Fixing unzipping ODM resources in build.sh. (1)
    
    Fixing unzipping ODM resources in build.sh. (2)
    
    Fixing unzipping ODM resources in build.sh. (3)

--- build.sh
+++ build.sh
@@ -6,10 +6,13 @@ cd ../
 
 echo "download odm distribution..."
 AUTH_TOKEN=$(curl -i -H "X-Auth-User: $AUTH_USER" -H "X-Auth-Key:$AUTH_KEY" https://dal05.objectstorage.softlayer.net/auth/v1.0 | grep -E "X-Auth-Token:" | awk {'print $2'})
-curl -O -H "X-Auth-Token: $AUTH_TOKEN" $ODM_URL > $HOME/CACHE/$ODM_FILE_NAME
+curl -O -H "X-Auth-Token: $AUTH_TOKEN" $ODM_URL 
+#-o $HOME/CACHE/$ODM_FILE_NAME
 
+#ls -R $HOME/CACHE
 echo "unzipp odm distribution..."
-unzip $HOME/CACHE/$ODM_FILE_NAME
+unzip $ODM_FILE_NAME # $HOME/CACHE/$ODM_FILE_NAME 
+# -d .
 
 echo "copy odm-ondocker into ODM distribution..."
 cp -R odm-ondocker install
