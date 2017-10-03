769f95ab533e95830cbcc062dedfe6f6f6924102
laurent grateau <laurent.grateau@fr.ibm.com>
2017-10-03 17:39:25 +0200

    Fix Service Name - odm-

--- decisionserver/decisionrunner/script/rundr.sh
+++ decisionserver/decisionrunner/script/rundr.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 if [ ! -f /config/initialized.flag ] ; then
 	cd  /config/apps/DecisionRunner.war/WEB-INF/classes;
-	sed -i 's/protocol=jmx/protocol=tcpip,tcpip.port='1883',tcpip.host='decisionserverconsole',tcpip.retryInterval=1000/' ra.xml;
+	sed -i 's/protocol=jmx/protocol=tcpip,tcpip.port='1883',tcpip.host='odm-decisionserverconsole',tcpip.retryInterval=1000/' ra.xml;
 	sed -i 's|<config-property-value>FINE</config-property-value>|<config-property-value>WARNING</config-property-value>|g' ra.xml;
 	sed -i '\#<config-property-name>DefaultConnectionManagerProperties#,\#<config-property-value/># s|<config-property-value/>|<config-property-value>pool.maxSize='$CONNECTION_POOL_SIZE',pool.waitTimeout=3000</config-property-value>|' ra.xml;
     sed -i 's|{pluginClass=ilog.rules.res.decisionservice.plugin.IlrWsdlGeneratorPlugin}|{pluginClass=ilog.rules.res.decisionservice.plugin.IlrWsdlGeneratorPlugin},{pluginClass=Metering}|g' ra.xml;
 	touch /config/initialized.flag
 fi;
-
+if [ -n "$DBSERVER_NAME" ] 
+then 
+	sed -i 's|dbserver|'$DBSERVER_NAME'|g' /config/datasource.xml
+fi
 /opt/ibm/docker/docker-server run defaultServer
--- decisionserver/decisionserverruntime/script/runds.sh
+++ decisionserver/decisionserverruntime/script/runds.sh
@@ -2,16 +2,21 @@
 set -x
 if [ ! -f /config/initialized.flag ] ; then
 	cd  /config/apps/DecisionService.war/WEB-INF/classes;
-	sed -i 's/protocol=jmx/protocol=tcpip,tcpip.port='1883',tcpip.host='decisionserverconsole',tcpip.retryInterval=1000/' ra.xml;
+	sed -i 's/protocol=jmx/protocol=tcpip,tcpip.port='1883',tcpip.host='odm-decisionserverconsole',tcpip.retryInterval=1000/' ra.xml;
 	sed -i 's|<config-property-value>FINE</config-property-value>|<config-property-value>WARNING</config-property-value>|g' ra.xml;
 	sed -i '\#<config-property-name>DefaultConnectionManagerProperties#,\#<config-property-value/># s|<config-property-value/>|<config-property-value>pool.maxSize='$CONNECTION_POOL_SIZE',pool.waitTimeout=3000</config-property-value>|' ra.xml;	
 	touch /config/initialized.flag
+fi;
+
+if [ -n "$DBSERVER_NAME" ] 
+then 
+	sed -i 's|dbserver|'$DBSERVER_NAME'|g' /config/datasource.xml
 fi;
 
 if [ -n "$COM_IBM_RULES_METERING_ENABLE" ] 
 then 
 	cd  /config/apps/DecisionService.war/WEB-INF/classes;
 	sed -i 's/{pluginClass=HTDS}/{pluginClass=Metering,enable=true},{pluginClass=DVS},{pluginClass=HTDS}/g' 	ra.xml
-fi
+fi;
 
 /opt/ibm/docker/docker-server run defaultServer
