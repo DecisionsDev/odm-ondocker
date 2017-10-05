b2474ebeb2310fd6d38ad7cc05a1583ad56f3aad
Jeremy Pichon <jeremy.pichon@fr.ibm.com>
2017-10-05 15:36:33 +0200

    Check if internal networks bridge are available (image a can access image b).
    Check if internal networks bridge are available (image a can access image b). 2
    
    Check if internal networks bridge are available (image a can access image b). 3
    
    Check if internal networks bridge are available (image a can access image b). 4
    
    Check if internal networks bridge are available (image a can access image b). 5
    
    Check if internal networks bridge are available (image a can access image b). 6
    
    Check if internal networks bridge are available (image a can access image b). 6

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ pwd
 cd ../
 
 if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
-	echo "ODM distribution: Starting download..."
-	AUTH_TOKEN=$(curl -i -H "X-Auth-User: $AUTH_USER" -H "X-Auth-Key:$AUTH_KEY" https://dal05.objectstorage.softlayer.net/auth/v1.0 | grep -E "X-Auth-Token:" | awk {'print $2'})
-	curl -O -H "X-Auth-Token: $AUTH_TOKEN" $ODM_URL 
-	mv $ODM_FILE_NAME $HOME/.cache/
-	echo "ODM distribution: download finished..."
+    echo "ODM distribution: Starting download..."
+    AUTH_TOKEN=$(curl -i -H "X-Auth-User: $AUTH_USER" -H "X-Auth-Key:$AUTH_KEY" https://dal05.objectstorage.softlayer.net/auth/v1.0 | grep -E "X-Auth-Token:" | awk {'print $2'})
+    curl -O -H "X-Auth-Token: $AUTH_TOKEN" $ODM_URL 
+    mv $ODM_FILE_NAME $HOME/.cache/
+    echo "ODM distribution: download finished..."
 else
-	echo "ODM distribution: Loading from cache..."
-	echo "ODM distribution: Loading finished..."
+    echo "ODM distribution: Loading from cache..."
+    echo "ODM distribution: Loading finished..."
 fi
 
 echo "unzipp odm distribution..."
--- test/wait-for-url.sh
+++ test/wait-for-url.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo "Testing url $1 availabilty."
+
 i=0
 until $(curl --connect-timeout 180 --output /dev/null --silent --head --fail $1); do
     i=$((i+1))
     if [[ $i -gt 10 ]]; then
-    	exit 1
+        printf 'X'
+        exit 1
     fi
     printf '.'
     sleep 10
 done
+
+printf "OK"
