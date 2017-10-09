291ecc252fe43fbfb8226dc15b54a24543b48a7a
Jeremy Pichon <jds.pichon@gmail.com>
2017-10-09 11:15:38 +0200

    Adding Travis build. #69
    Adding Travis build.

--- build.sh
+++ build.sh
@@ -4,12 +4,19 @@ echo "current build directory:"
 pwd
 cd ../
 
-echo "download odm distribution..."
-AUTH_TOKEN=$(curl -i -H "X-Auth-User: $AUTH_USER" -H "X-Auth-Key:$AUTH_KEY" https://dal05.objectstorage.softlayer.net/auth/v1.0 | grep -E "X-Auth-Token:" | awk {'print $2'})
-curl -O -H "X-Auth-Token: $AUTH_TOKEN" $ODM_URL
+if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
+    echo "ODM distribution: Starting download..."
+    AUTH_TOKEN=$(curl -i -H "X-Auth-User: $AUTH_USER" -H "X-Auth-Key:$AUTH_KEY" https://dal05.objectstorage.softlayer.net/auth/v1.0 | grep -E "X-Auth-Token:" | awk {'print $2'})
+    curl -O -H "X-Auth-Token: $AUTH_TOKEN" $ODM_URL 
+    mv $ODM_FILE_NAME $HOME/.cache/
+    echo "ODM distribution: download finished..."
+else
+    echo "ODM distribution: Loading from cache..."
+    echo "ODM distribution: Loading finished..."
+fi
 
-echo "unzip odm distribution..."
-unzip -q $ODM_FILE_NAME
+echo "unzipp odm distribution..."
+unzip -q $HOME/.cache/$ODM_FILE_NAME -d .
 
 echo "copy odm-ondocker into ODM distribution..."
 cp -R odm-ondocker install
---
+++ test/wait-for-url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Testing url $1 availabilty."
+
+i=0
+until $(curl --connect-timeout 180 --output /dev/null --silent --head --fail $1); do
+    i=$((i+1))
+    if [[ $i -gt 10 ]]; then
+        printf 'X'
+        exit 1
+    fi
+    printf '.'
+    sleep 15
+done
+
+printf "OK"
