3a85442fc544d5d43eb86f48109faa9c93b0dedd
Yiquan Zhou <yiquan.zhou@fr.ibm.com>
2017-10-10 15:52:37 +0200

    close #50, enable basic auth in decision server runtime, add resExecutor role

--- decisionserver/decisionserverruntime/script/runds.sh
+++ decisionserver/decisionserverruntime/script/runds.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -x
 if [ ! -f /config/initialized.flag ] ; then
+	cd /config/apps/DecisionService.war/WEB-INF;
+	sed -i $'/<\/web-app>/{e cat /config/basicAuth.xml\n}' web.xml
+
 	cd  /config/apps/DecisionService.war/WEB-INF/classes;
 	sed -i 's/protocol=jmx/protocol=tcpip,tcpip.port='1883',tcpip.host='odm-decisionserverconsole',tcpip.retryInterval=1000/' ra.xml;
 	sed -i 's|<config-property-value>FINE</config-property-value>|<config-property-value>WARNING</config-property-value>|g' ra.xml;
@@ -10,7 +13,7 @@ fi;
 
 
 if [ -n "$DBSERVER_NAME" ]
-then 
+then
 	sed -i 's|dbserver|'$DBSERVER_NAME'|g' /config/datasource.xml
 fi;
 if [ -n "$DECISIONSERVERCONSOLE_NAME" ]
