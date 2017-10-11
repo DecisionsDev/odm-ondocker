94d9d095edb48840f8cd7f080f3b8ed8623e9ca8
Yiquan Zhou <yiquan.zhou@fr.ibm.com>
2017-10-11 09:58:23 +0200

    add authentication to wait-for-url script

--- test/wait-for-url.sh
+++ test/wait-for-url.sh
@@ -2,8 +2,14 @@
 
 echo "Testing url $1 availabilty."
 
+if [ $# -ge 3 ]
+then
+  echo "authentication is enabled."
+  auth="-u $2:$3"
+fi
+
 i=0
-until $(curl --connect-timeout 180 --output /dev/null --silent --head --fail $1); do
+until $(curl $auth --connect-timeout 180 --output /dev/null --silent --head --fail $1); do
     i=$((i+1))
     if [[ $i -gt 10 ]]; then
         printf 'X'
