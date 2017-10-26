e13f1ddae8ec33049fda3973ebc9d150d8563504
Rachel Orti <rachel.orti@fr.ibm.com>
2017-10-26 19:08:09 +0200

    Resolves #86 - Use H2 instead of Derby for Standalone + add PostgreSQL.

--- standalone/script/runserver.sh
+++ standalone/script/runserver.sh
@@ -3,7 +3,6 @@
 if [ ! -f /config/initializeddb.flag ] ; then
 	if [ "$SAMPLE" = "true" ] ; then
 		if [ -d "/config/dbdata/" ]; then
-  		# Control will enter here if $DIRECTORY exists.
   			cp -R /upload/* /config/dbdata/
 		fi;
 	fi;
@@ -17,11 +16,41 @@ if [ ! -f /config/initialized.flag ] ; then
 	touch /config/initialized.flag
 fi;
 
-
 if [ -n "$COM_IBM_RULES_METERING_ENABLE" ] 
 then 
 	cd  /config/apps/DecisionService.war/WEB-INF/classes;
 	sed -i 's/{pluginClass=HTDS}/{pluginClass=Metering,enable=true},{pluginClass=DVS},{pluginClass=HTDS}/g' 	ra.xml
+fi
+
+# Begin - Configuration for the database
+if [ -n "$DB_DRIVER_URL" ]
+then
+	echo "Use DB_DRIVER_URL: $DB_DRIVER_URL"
+	wget -nv $DB_DRIVER_URL
+    case $DB_DRIVER_URL in
+      	*postgres* ) rm /config/resources/postgres*
+					 mv postgres* /config/resources 
+					 cp /config/datasource-postgres.xml /config/datasource.xml 
+					 ;; 
+	esac
+elif [ -n "$DB_TYPE" ]
+then
+	echo "Use DB_TYPE: $DB_TYPE"
+	case $DB_TYPE in
+		*h2* ) cp /config/datasource-h2.xml /config/datasource.xml 
+			   ;; 
+      	*postgres* ) cp /config/datasource-postgres.xml /config/datasource.xml 
+					 ;; 
+	esac
+else
+	echo "Use H2 as database by default"
+	cp /config/datasource-h2.xml /config/datasource.xml
+fi
+# End - Configuration for the database
+
+if [ -n "$DBSERVER_NAME" ] 
+then 
+	sed -i 's|dbserver|'$DBSERVER_NAME'|g' /config/datasource.xml
 fi
 
 /opt/ibm/docker/docker-server run defaultServer
