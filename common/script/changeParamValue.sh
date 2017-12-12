0e0dff64a3283b76eafc3deaeee93a3127996189
Yiquan Zhou <yiquan.zhou@fr.ibm.com>
2017-12-12 17:25:41 +0100

    move features script to common folder, use changeParamValue in docker files

--- common/script/changeParamValue.sh
+++ common/script/changeParamValue.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# parameters:
-# $1 the param-name
-# $2 old value
-# $3 new value
-# $4 the web.xml file path
+paramName=$1
+oldValue=$2
+newValue=$3
+file=$4
 
-echo "change the value of param $1 from $2 to $3 in $4"
+echo "change the value of param $paramName from $oldValue to $newValue in $file"
 
-perl -i -p0e "s/(<param-name>$1<\/param-name>\s*<param-value>)($2)(<\/param-value>)/\1$3\3/s" $4
+# . matches any value
+if [ $oldValue = "." ]
+then
+  oldValue=".*?"
+fi
+
+perl -i -p0e "s/(<param-name>$paramName<\/param-name>\s*<param-value>)($oldValue)(<\/param-value>)/\1$newValue\3/s" $file
