b5a4565642e288d45431dae5b28d28301c185819
Rachel Orti <rachel.orti@fr.ibm.com>
2018-01-17 14:43:45 +0100

    Fix #131 - Add support for trust store

--- common/script/configureTlsSecurity.sh
+++ common/script/configureTlsSecurity.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Begin - Configuration for the tls security
-echo "Configure the tls keystore password"
+# Begin - Configuration for the TLS security
+echo "Configure the TLS keystore password"
 if [ -n "$KEYSTORE_PASSWORD" ]
 then
-	sed -i 's|__PASSWORD__|'$KEYSTORE_PASSWORD'|g' /config/tlsSecurity.xml
+	sed -i 's|__KEYSTORE_PASSWORD__|'$KEYSTORE_PASSWORD'|g' /config/tlsSecurity.xml
 else
-	sed -i 's|__PASSWORD__|'changeme'|g' /config/tlsSecurity.xml
+	sed -i 's|__KEYSTORE_PASSWORD__|'changeme'|g' /config/tlsSecurity.xml
 fi
-# End - Configuration for the tls security
+echo "Configure the TLS truststore password"
+if [ -n "$TRUSTSTORE_PASSWORD" ]
+then
+	sed -i 's|__TRUSTSTORE_PASSWORD__|'$TRUSTSTORE_PASSWORD'|g' /config/tlsSecurity.xml
+else
+	sed -i 's|__TRUSTSTORE_PASSWORD__|'changeme'|g' /config/tlsSecurity.xml
+fi
+# End - Configuration for the TLS security
