9b0a16ea65a85fd97e9b907804c966146d4f2d99
Yiquan Zhou <yiquan.zhou@fr.ibm.com>
2018-01-25 10:14:58 +0100

    fix decision service url, add error messages

--- decisionserver/decisionserverconsole/script/updateDSCConfigurations.sh
+++ decisionserver/decisionserverconsole/script/updateDSCConfigurations.sh
@@ -2,9 +2,9 @@
 
 echo "Update Decision Server Console configurations"
 
-if [ -n "$DECISION_SERVICE_URL" ]
-then
-	sed -i 's|/DecisionService|'$DECISION_SERVICE_URL'|g' $APPS/res.war/WEB-INF/web.xml
-else
-	sed -i 's|/DecisionService|http://localhost:9090/DecisionService|g' $APPS/res.war/WEB-INF/web.xml
+if [ -z "$DECISION_SERVICE_URL" ]; then
+	echo "ERROR: The environment variable DECISION_SERVICE_URL is not configured. It specifies the URL of Decision Service Runtime in Decision Service Console."
+	exit 1
 fi
+
+sed -i 's|/DecisionService|'$DECISION_SERVICE_URL'|g' $APPS/res.war/WEB-INF/web.xml
--- decisionserver/decisionserverruntime/script/configureTcpipNotification.sh
+++ decisionserver/decisionserverruntime/script/configureTcpipNotification.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-tcpipHost="odm-decisionserverconsole"
 tcpipPort=1883
 
 if [ -n "$DECISIONSERVERCONSOLE_NAME" ]
 then
   tcpipHost="$DECISIONSERVERCONSOLE_NAME"
 else
-  echo "The environment variable DECISIONSERVERCONSOLE_NAME is not configured. The tcpip host is set to default value $tcpipHost"
+  echo "ERROR: The environment variable DECISIONSERVERCONSOLE_NAME is not configured. It specifies the host name of Decision Server Consolethat the tcpip notification plugin of Decision Server Runtime connects to."
+  exit 1
 fi
 
 echo "Enable tcpip notification between Decision Server Runtime XU and Decision Server Console $tcpipHost on port $tcpipPort"
