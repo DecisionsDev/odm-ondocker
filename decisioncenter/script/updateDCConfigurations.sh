a11ae1db0abf386419cd739f8ffff18a8e9ec9d3
mmouly <mathias.mouly@fr.ibm.com>
2018-04-10 17:35:18 +0200

    #151 Adapt configuration to new jsonified Decision Center servers definition

--- decisioncenter/script/updateDCConfigurations.sh
+++ decisioncenter/script/updateDCConfigurations.sh
@@ -2,41 +2,58 @@
 
 echo "Update Decision Center configurations"
 
+if [ -n "$DC_SERVER_CONFIG" ]
+then
+  echo "DC_SERVER_CONFIG set to $DC_SERVER_CONFIG"
+else
+  echo "DC_SERVER_CONFIG not set. Use old way Decision Center server configuration" 	
+	DC_SERVER_CONFIG="/config/apps/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties"
+fi
+
+if [ "$DC_SERVER_CONFIG" == "/config/server-configurations.json" ]
+then
+  echo "Update decisioncenter-configuration.properties with /config/server-configurations.json"
+	cp /config/new-decisioncenter-configuration.properties $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+else
+  echo "Use old decisioncenter-configuration.properties definition"
+	cp /config/decisioncenter-configuration.properties $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+fi
+
 if [ -n "$DECISIONSERVERCONSOLE_NAME" ]
 then
-  echo "Update decision server console name to $DECISIONSERVERCONSOLE_NAME in decisioncenter-configuration.properties"
-	sed -i 's|odm-decisionserverconsole|'$DECISIONSERVERCONSOLE_NAME'|g' $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+  echo "Update decision server console name to $DECISIONSERVERCONSOLE_NAME in $DC_SERVER_CONFIG"
+	sed -i 's|odm-decisionserverconsole|'$DECISIONSERVERCONSOLE_NAME'|g' $DC_SERVER_CONFIG
 fi
 if [ -n "$DECISIONRUNNER_NAME" ]
 then
-  echo "Update decision runner name to $DECISIONRUNNER_NAME in decisioncenter-configuration.properties"
-	sed -i 's|odm-decisionrunner|'$DECISIONRUNNER_NAME'|g' $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+  echo "Update decision runner name to $DECISIONRUNNER_NAME in $DC_SERVER_CONFIG"
+	sed -i 's|odm-decisionrunner|'$DECISIONRUNNER_NAME'|g' $DC_SERVER_CONFIG
 fi
 
 if [ -n "$ENABLE_TLS" ]
 then
- echo "Update decision server and runner protocol to https in decisioncenter-configuration.properties"
-        sed -i 's|protocol|'https'|g' $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+ echo "Update decision server and runner protocol to https in $DC_SERVER_CONFIG"
+        sed -i 's|protocol|'https'|g' $DC_SERVER_CONFIG
 else
- echo "Update decision server and runner protocol to http in decisioncenter-configuration.properties"
-        sed -i 's|protocol|'http'|g' $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+ echo "Update decision server and runner protocol to http in $DC_SERVER_CONFIG"
+        sed -i 's|protocol|'http'|g' $DC_SERVER_CONFIG
 fi
 
 if [ -n "$DECISIONSERVERCONSOLE_PORT" ]
 then
-  echo "Update decision server console port to $DECISIONSERVERCONSOLE_PORT in decisioncenter-configuration.properties"
-        sed -i 's|decisionserverconsole-port|'$DECISIONSERVERCONSOLE_PORT'|g' $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+  echo "Update decision server console port to $DECISIONSERVERCONSOLE_PORT in $DC_SERVER_CONFIG"
+        sed -i 's|decisionserverconsole-port|'$DECISIONSERVERCONSOLE_PORT'|g' $DC_SERVER_CONFIG
 else
-  echo "Update decision server console port to default 9080 in decisioncenter-configuration.properties"
-        sed -i 's|decisionserverconsole-port|'9080'|g' $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+  echo "Update decision server console port to default 9080 in $DC_SERVER_CONFIG"
+        sed -i 's|decisionserverconsole-port|'9080'|g' $DC_SERVER_CONFIG
 fi
 if [ -n "$DECISIONRUNNER_PORT" ]
 then
-  echo "Update decision runner port to $DECISIONRUNNER_PORT in decisioncenter-configuration.properties"
-        sed -i 's|decisionrunner-port|'$DECISIONRUNNER_PORT'|g' $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+  echo "Update decision runner port to $DECISIONRUNNER_PORT in $DC_SERVER_CONFIG"
+        sed -i 's|decisionrunner-port|'$DECISIONRUNNER_PORT'|g' $DC_SERVER_CONFIG
 else
-  echo "Update decision runner port to default 9080 in decisioncenter-configuration.properties"
-        sed -i 's|decisionrunner-port|'9080'|g' $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+  echo "Update decision runner port to default 9080 in $DC_SERVER_CONFIG"
+        sed -i 's|decisionrunner-port|'9080'|g' $DC_SERVER_CONFIG
 fi
 
 if [ -s "/config/auth/ldap-configurations.xml" ] 
