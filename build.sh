3af9b7fd935606a2b537bd237d9a73ae122de254
Jeremy Pichon <jeremy.pichon@fr.ibm.com>
2018-05-22 15:00:53 +0200

    Editing build.sh to hide ODM_URL

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd ../
 
 if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
     echo "ODM distribution: Starting download..."
-    ODM_ZIP_URL=https://hyc-odm-docker.artifactory.swg-devops.com/artifactory/hyc-odm-snapshot-maven-local/com/ilog/rules/cloud/docker-compose-build-images/${ODM_VERSION}/docker-compose-build-images-${ODM_VERSION}.zip
+    ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
     curl  $ODM_ZIP_URL -u ${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD} -o $ODM_FILE_NAME
     mv $ODM_FILE_NAME $HOME/.cache/
     echo "ODM distribution: download finished..."
