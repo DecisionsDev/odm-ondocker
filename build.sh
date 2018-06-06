39ebe79eccd4c725544509098b50945725c33afc
laurent grateau <laurent.grateau@fr.ibm.com>
2018-06-06 13:26:38 +0200

    Attempt to fix build

--- build.sh
+++ build.sh
@@ -6,11 +6,9 @@ pwd
 cd ../
 
 if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
-    echo "ODM distribution: Starting download... ${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip"
-    PASSDEC=`echo ${ARTIFACTORY_PASSWORD}|base64 --decode`
+    echo "ODM distribution: Starting download..."
     ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
-    curl  $ODM_ZIP_URL -u '${ARTIFACTORY_USER}:$PASSDEC' -o $ODM_FILE_NAME
-    tail -100 $ODM_FILE_NAME
+    curl  $ODM_ZIP_URL -u '${ARTIFACTORY_USER}:${AUTHPWD}' -o $ODM_FILE_NAME
     mv $ODM_FILE_NAME $HOME/.cache/
     echo "ODM distribution: download finished..."
 else
