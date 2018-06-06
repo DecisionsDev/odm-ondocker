d9b98cf28eadf6762bf2ce5f382182c8498b0b83
laurent grateau <laurent.grateau@fr.ibm.com>
2018-06-06 14:27:16 +0200

    no message

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ cd ../
 if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
     echo "ODM distribution: Starting download..."
     ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
-    curl  $ODM_ZIP_URL -u '${ARTIFACTORY_USER}:${ARTIPWD}' -o $ODM_FILE_NAME
+    curl  $ODM_ZIP_URL -u '${ARTIFACTORY_USER}:${ARTIPWD}' -o $ODM_FILE_NAME 
+    echo "curl  $ODM_ZIP_URL -u '${ARTIFACTORY_USER}:${ARTIPWD}' -o $ODM_FILE_NAME"
     mv $ODM_FILE_NAME $HOME/.cache/
     echo "ODM distribution: download finished..."
 else
