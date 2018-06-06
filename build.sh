4c8c92a3cd62494a4c5a2331c57ef3a15c64e639
laurent grateau <laurent.grateau@fr.ibm.com>
2018-06-06 18:16:53 +0200

    Finalize the fix

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-set -x
 echo "start building odm-ondocker..."
 echo "current build directory:"
 pwd
@@ -9,7 +8,7 @@ cd ../
 if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
     echo "ODM distribution: Starting download..."
     ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
-    curl  $ODM_ZIP_URL -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PWD}" -o $ODM_FILE_NAME -v
+    curl  $ODM_ZIP_URL -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PWD}" -o $ODM_FILE_NAME
     mv $ODM_FILE_NAME $HOME/.cache/
     echo "ODM distribution: download finished..."
 else
