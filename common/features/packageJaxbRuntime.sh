2aaa80e9d3fc95bba33ed8e01a79a87cdd11f067
laurent grateau <laurent.grateau@fr.ibm.com>
2018-07-04 14:53:07 +0200

    Try to fix the remaining issue for Liberty 18.0.0.2

--- common/features/packageJaxbRuntime.sh
+++ common/features/packageJaxbRuntime.sh
@@ -3,11 +3,27 @@
 set -x
 echo "running packageJaxbRuntime.sh "
 
-WLPPATTERN="$APPS/res.war/WEB-INF/lib/jaxb*.jar"
-if  ! ls $WLPPATTERN 1> /dev/null 2>&1;  then
-  if [ -d "$APPS/res.war" ] && [ -d "$APPS/DecisionService.war" ]; then
+# Process RES Console if needed
+if [ -d "$APPS/res.war" ] && [ -d "$APPS/DecisionService.war" ]; then
+JAXBPATTERN="$APPS/res.war/WEB-INF/lib/jaxb*.jar"
+  if  ! ls $JAXBPATTERN 1> /dev/null 2>&1;  then
     cp $APPS/DecisionService.war/WEB-INF/lib/jaxb*.jar $APPS/res.war/WEB-INF/lib
-  else
-    fail "Jaxb could not be repackaged"
+  fi
+fi
+
+# Process Decision Runner if needed
+if [ -d "$APPS/DecisionRunner.war" ]; then
+  JAXBPATTERN="$APPS/DecisionRunner.war/WEB-INF/lib/jaxb*.jar"
+  if  ! ls $JAXBPATTERN 1> /dev/null 2>&1;  then
+    cp $THIRDPARTY/jaxb*.jar $APPS/DecisionRunner.war/WEB-INF/lib
+  fi
+fi
+
+# Process Decision Center if needed
+if [ -d "$APPS/decisioncenter.war" ]; then
+  JAXBPATTERN="$APPS/decisioncenter.war/WEB-INF/lib/jaxb*.jar"
+  if  ! ls $JAXBPATTERN 1> /dev/null 2>&1;  then
+    cp $THIRDPARTY/jaxb*.jar $APPS/decisioncenter.war/WEB-INF/lib
+    cp $THIRDPARTY/jaxb*.jar $APPS/teamserver.war/WEB-INF/lib
   fi
 fi
