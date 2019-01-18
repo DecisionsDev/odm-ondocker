5212360543710f5c50bb79ac15fdd5a4801b55c3
laurent grateau <laurent.grateau@fr.ibm.com>
2019-01-18 17:47:57 +0100

    Integration of bai event emitter in the dsr inside the dsc

--- decisionserver/decisionserverconsole/script/updateDSCConfigurations.sh
+++ decisionserver/decisionserverconsole/script/updateDSCConfigurations.sh
@@ -4,3 +4,8 @@ if [ -n "$DECISION_SERVICE_URL" ]; then
 	echo "Update DECISION_SERVICE_URL to $DECISION_SERVICE_URL in Decision Server Console."
 	sed -i 's|/DecisionService|'$DECISION_SERVICE_URL'|g' $APPS/res.war/WEB-INF/web.xml
 fi
+
+if [ -f "/config/baiemitterconfig/plugin-configuration.properties" ]; then
+	echo "Enable BAI Emitter Plugin"
+	perl -i -p0e "s/({pluginClass=HTDS[^}]*)/\1},{pluginClass=ODMEmitterForBAI/gm" ra.xml;
+fi
