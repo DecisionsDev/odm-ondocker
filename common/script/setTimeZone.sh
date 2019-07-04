10360196aad2de4a32265f54ad0652fcdbe6ffa0
mmouly <mathias.mouly@fr.ibm.com>
2019-07-04 09:43:08 +0200

    remove message if TZ not set as inconsistent with prod chart config map

--- common/script/setTimeZone.sh
+++ common/script/setTimeZone.sh
@@ -4,6 +4,4 @@ if [ -n "$TZ" ]
 then
         echo "Use TimeZone set to $TZ"
         sed -i 's|Europe/Paris|'$TZ'|g' /config/configDropins/overrides/jvm.options
-else
-  echo "No TimeZone set use default Europe/Paris"
 fi
