c102dae0cce1c236db963abbbdc276a91f21f7f9
mmouly <mathias.mouly@fr.ibm.com>
2019-11-05 17:54:39 +0100

    change decisionserverconsole config

--- decisionserver/decisionserverconsole/script/updateDSCConfigurations.sh
+++ decisionserver/decisionserverconsole/script/updateDSCConfigurations.sh
@@ -37,11 +37,15 @@ then
   sed -i '/<group name="resExecutors"/d' /config/application.xml
 
   echo "Enable UMS authentication"
-  UMS_LOGOUT_URL=$(grep UMS_LOGOUT_URL /config/auth/openIdParameters.txt | sed "s/UMS_LOGOUT_URL=//g")
-  echo "UMS_LOGOUT_URL: $UMS_LOGOUT_URL"
   UMS_ALLOWED_DOMAINS=$(grep UMS_ALLOWED_DOMAINS /config/auth/openIdParameters.txt | sed "s/UMS_ALLOWED_DOMAINS=//g")
   echo "UMS_ALLOWED_DOMAINS: $UMS_ALLOWED_DOMAINS"
-  sed -i 's|type=local|'type=openid,logoutUrl=$UMS_LOGOUT_URL'|g' $APPS/res.war/WEB-INF/web.xml
+  UMS_LOGOUT_URL=$(grep UMS_LOGOUT_URL /config/auth/openIdParameters.txt | sed "s/UMS_LOGOUT_URL=//g")
+  if [ -n "$UMS_LOGOUT_URL" ]; then
+  	echo "UMS_LOGOUT_URL: $UMS_LOGOUT_URL"
+	sed -i 's|type=local|'type=openid,logoutUrl=$UMS_LOGOUT_URL'|g' $APPS/res.war/WEB-INF/web.xml
+  else
+	sed -i 's|type=local|'type=openid'|g' $APPS/res.war/WEB-INF/web.xml
+  fi
   sed -i 's|UMS_ALLOWED_DOMAINS|'$UMS_ALLOWED_DOMAINS'|g' /config/oAuth.xml
   sed -i $'/<\/web-app>/{e cat /config/oAuth.xml\n}' $APPS/res.war/WEB-INF/web.xml
 else
