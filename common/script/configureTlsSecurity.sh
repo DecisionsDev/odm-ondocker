fa6b0de9864825e5409286b2f000f8b625973d32
mathias-mouly <mathias.mouly@fr.ibm.com>
2019-12-05 14:33:59 +0100

    change certificate management for DBA

--- common/script/configureTlsSecurity.sh
+++ common/script/configureTlsSecurity.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+export DEFAULT_KEYSTORE_PASSWORD=changeme
+export DEFAULT_TRUSTSTORE_PASSWORD=changeme
+
+if [ -s "/shared/tls/keystore/jks/server.jks" ]
+then
+	echo "replace /config/security/keystore.jks by /shared/tls/keystore/jks/server.jks and default keystore password"
+	cp /shared/tls/keystore/jks/server.jks /config/security/keystore.jks
+	DEFAULT_KEYSTORE_PASSWORD=changeit
+fi
+
+if [ -s "/shared/tls/keystore/jks/server.jks" ]
+then
+	echo "replace /config/security/trustore.jks by /shared/tls/truststore/jks/trust.jks and default keystore password"
+	cp /shared/tls/truststore/jks/trust.jks /config/security/truststore.jks
+	DEFAULT_TRUSTSTORE_PASSWORD=changeit
+fi
+
 # Begin - Configuration for the TLS security
 echo "Configure the TLS keystore password"
 if [ -n "$KEYSTORE_PASSWORD" ]
 then
 	sed -i 's|__KEYSTORE_PASSWORD__|'$KEYSTORE_PASSWORD'|g' /config/tlsSecurity.xml
 else
-	sed -i 's|__KEYSTORE_PASSWORD__|'changeme'|g' /config/tlsSecurity.xml
+	sed -i 's|__KEYSTORE_PASSWORD__|'$DEFAULT_KEYSTORE_PASSWORD'|g' /config/tlsSecurity.xml
 fi
 echo "Configure the TLS truststore password"
 if [ -n "$TRUSTSTORE_PASSWORD" ]
 then
 	sed -i 's|__TRUSTSTORE_PASSWORD__|'$TRUSTSTORE_PASSWORD'|g' /config/tlsSecurity.xml
 else
-	sed -i 's|__TRUSTSTORE_PASSWORD__|'changeme'|g' /config/tlsSecurity.xml
+	sed -i 's|__TRUSTSTORE_PASSWORD__|'$DEFAULT_TRUSTSTORE_PASSWORD'|g' /config/tlsSecurity.xml
 fi
 # End - Configuration for the TLS security
