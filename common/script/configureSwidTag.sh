64a9643c7978920e79f126c7c84d9280156d02dc
mathias-mouly <mathias.mouly@fr.ibm.com>
2020-05-19 19:44:24 +0200

    prod swidtag management

--- common/script/configureSwidTag.sh
+++ common/script/configureSwidTag.sh
@@ -23,20 +23,20 @@ function removeAllSwidTag () {
 if [ -n "$KubeVersion" ]; then
         if [[ $KubeVersion =~ "icp" ]] || [[ $KubeVersion =~ "ODM on K8s" ]]; then
                 echo "ODM configuration : remove all DBAMC Swidtag"
-		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_*.swidtag
-		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto_-_Non_Prod*.swidtag
+		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto-*.swidtag
+		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto_-_Non_Prod-*.swidtag
         else
 		if [ -n "$DEPLOY_FOR_PRODUCTION" ]; then
         		if [[ "$DEPLOY_FOR_PRODUCTION" == "TRUE" ]]; then
                 		echo "DEPLOY_FOR_PRODUCTION is true then DBAMC production configuration : remove DBAMC non production Swidtag"
-                		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto_-_Non_Prod*.swidtag
+                		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto_-_Non_Prod-*.swidtag
         		else
                 		echo "DEPLOY_FOR_PRODUCTION is false then DBAMC non production configuration : remove DBAMC production Swidtag"
-                		removeAllSwidTag swidtagToRemove=ibm.com_Operational_Decision_Manager_Containers_*.swidtag
+                		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto-*.swidtag
         		fi
 		else
 			echo "DEPLOY_FOR_PRODUCTION not set then DBAMC production configuration : remove DBAMC non production Swidtag"
-        		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto_-_Non_Prod*.swidtag
+        		removeAllSwidTag ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto_-_Non_Prod-*.swidtag
                 fi
 		
                 echo "DBAMC configuration : remove all ODM Swidtag"
