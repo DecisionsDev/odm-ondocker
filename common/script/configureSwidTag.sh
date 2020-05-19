f7cd114dfe4d17935b099929a3b251b4e901c3b6
mathias-mouly <mathias.mouly@fr.ibm.com>
2020-05-19 13:38:59 +0200

    prod swidtag management

--- common/script/configureSwidTag.sh
+++ common/script/configureSwidTag.sh
@@ -8,15 +8,7 @@ function removeSwidTag () {
         fi
 }
 
-if [ -n "$KubeVersion" ]; then
-        if [[ $KubeVersion =~ "icp" ]] || [[ $KubeVersion =~ "ODM on K8s" ]]; then
-                echo "ODM configuration : remove all DBAMC Swidtag"
-                swidtagToRemove=ibm.com_IBM_Cloud_Pak_for_Automation*.swidtag
-        else
-                echo "DBAMC configuration : remove all ODM Swidtag"
-                swidtagToRemove=ibm.com_IBM_ODM_*.swidtag
-        fi
-
+function removeAllSwidTag () {
         removeSwidTag /config/apps/decisioncenter.war/META-INF/swidtag/$swidtagToRemove
         removeSwidTag /config/apps/decisionmodel.war/META-INF/swidtag/$swidtagToRemove
         removeSwidTag /config/apps/decisioncenter-api.war/META-INF/swidtag/$swidtagToRemove
@@ -25,4 +17,33 @@ if [ -n "$KubeVersion" ]; then
         removeSwidTag /config/apps/DecisionRunner.war/META-INF/swidtag/$swidtagToRemove
         removeSwidTag /config/apps/DecisionService.war/META-INF/swidtag/$swidtagToRemove
         removeSwidTag /config/apps/res.war/META-INF/swidtag/$swidtagToRemove
+}
+
+if [ -n "$KubeVersion" ]; then
+        if [[ $KubeVersion =~ "icp" ]] || [[ $KubeVersion =~ "ODM on K8s" ]]; then
+                echo "ODM configuration : remove all DBAMC Swidtag"
+                swidtagToRemove=ibm.com_Operational_Decision_Manager_Containers_*.swidtag
+        else
+                echo "DBAMC configuration : remove all ODM Swidtag"
+                swidtagToRemove=ibm.com_IBM_ODM_*.swidtag
+        fi
+
+	removeAllSwidTag
+
+fi
+
+if [ -n "$DEPLOY_FOR_PRODUCTION" ]; then
+        if [[ "$DEPLOY_FOR_PRODUCTION" == "TRUE" ]]; then
+                echo "DEPLOY_FOR_PRODUCTION is true then DBAMC production configuration : remove DBAMC non production Swidtag"
+                swidtagToRemove=ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto_-_Non_Prod*.swidtag
+        else
+                echo "DEPLOY_FOR_PRODUCTION is false then DBAMC non production configuration : remove DBAMC production Swidtag"
+                swidtagToRemove=ibm.com_Operational_Decision_Manager_Containers_*.swidtag
+        fi
+
+        removeAllSwidTag
+else
+	echo "DEPLOY_FOR_PRODUCTION not set then DBAMC production configuration : remove DBAMC non production Swidtag"
+        swidtagToRemove=ibm.com_Operational_Decision_Manager_Containers_-_CP4Auto_-_Non_Prod*.swidtag
+	removeAllSwidTag
 fi
