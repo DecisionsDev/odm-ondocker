81a8312c9fbdf3e3993953ad995b7746e7f8974e
Julie Garrone <julie.garrone@fr.ibm.com>
2020-07-06 10:46:19 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ cd ../
 # if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
     echo "ODM distribution: Starting download..."
     ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
-    curl  $ODM_ZIP_URL -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PWD}" -o $ODM_FILE_NAME
-    mv $ODM_FILE_NAME $HOME/.cache/
+    curl  "$ODM_ZIP_URL" -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PWD}" -o "$ODM_FILE_NAME"
+    mv "$ODM_FILE_NAME" "$HOME"/.cache/
     echo "ODM distribution: download finished..."
 #else
 #    echo "ODM distribution: Loading from cache..."
@@ -17,7 +17,7 @@ cd ../
 #fi
 
 echo "unzip odm distribution..."
-unzip -q $HOME/.cache/$ODM_FILE_NAME
+unzip -q "$HOME"/.cache/"$ODM_FILE_NAME"
 
 echo "copy odm-ondocker into ODM distribution..."
 cp -R odm-ondocker install
@@ -27,7 +27,7 @@ cp resources/.dockerignore ../
 # Optimizing the build to download webprofile package.
 source .env
 
-docker run --user 'root' -v $PWD/wlp:/opt/wlp  $FROMLIBERTY  /bin/sh -c "mkdir -p /opt/wlp ;\
+docker run --user 'root' -v "$PWD"/wlp:/opt/wlp  "$FROMLIBERTY"  /bin/sh -c "mkdir -p /opt/wlp ;\
  installUtility download openidconnectclient-1.0 collectiveMember-1.0 sessionCache-1.0 ldapRegistry-3.0 localConnector-1.0 \
   microProfile-1.0 microProfile-1.2 microProfile-1.3 microProfile-1.4 monitor-1.0 restConnector-1.0 \
   requestTiming-1.0 restConnector-2.0 sessionDatabase-1.0 ssl-1.0 transportSecurity-1.0 webCache-1.0 \
