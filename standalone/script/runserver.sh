b980fc4fbebcb71f336d732dd58ae1146e301ce3
Julie Garrone <julie.garrone@fr.ibm.com>
2020-07-07 16:02:59 +0200

    Fix standalone scripts

--- standalone/script/runserver.sh
+++ standalone/script/runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-$SCRIPT/checkLicense.sh
+"$SCRIPT"/checkLicense.sh
 
 if [ ! -f /config/initializeddb.flag ] ; then
     # if $SAMPLE not exist, or set to true
@@ -15,8 +15,8 @@ if [ ! -f /config/initializeddb.flag ] ; then
 fi;
 
 if [ ! -f /config/initialized.flag ] ; then
-	cd  $APPS/DecisionService.war/WEB-INF/classes;
-	sed -i 's|<config-property-value>FINE</config-property-value>|<config-property-value>WARNING</config-property-value>|g' ra.xml;
+	cd  "$APPS"/DecisionService.war/WEB-INF/classes || exit;
+	sed -i "s|<config-property-value>FINE</config-property-value>|<config-property-value>WARNING</config-property-value>|g" ra.xml;
 	sed -i '\#<config-property-name>[D|d]efaultConnectionManagerProperties#,\#<config-property-value/># s|<config-property-value/>|<config-property-value>pool.maxSize='$CONNECTION_POOL_SIZE',pool.waitTimeout=3000</config-property-value>|' ra.xml;
 
 	if [ -f "/config/baiemitterconfig/plugin-configuration.properties" ]; then
@@ -27,41 +27,41 @@ if [ ! -f /config/initialized.flag ] ; then
 	touch /config/initialized.flag
 fi;
 
-FIND_SERVER_EXT_CLASS="$($SCRIPT/findServerExtClass.sh)"
+FIND_SERVER_EXT_CLASS=$("$SCRIPT"/findServerExtClass.sh)
 echo "FIND_SERVER_EXT_CLASS set to $FIND_SERVER_EXT_CLASS"
 
 if [ "$FIND_SERVER_EXT_CLASS" == "matches" ]
 then
   echo "ServerExt class found. Use /config/server-configurations.json server definition"
-  cp /config/new-decisioncenter-configuration.properties $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+  cp /config/new-decisioncenter-configuration.properties "$APPS"/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
 else
   echo "ServerExt class not found. Use old decisioncenter-configuration.properties server definition"
-        cp /config/decisioncenter-configuration.properties $APPS/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
+        cp /config/decisioncenter-configuration.properties "$APPS"/decisioncenter.war/WEB-INF/classes/config/decisioncenter-configuration.properties
 fi
 
 if [ -n "$RELEASE_NAME" ]
 then
   echo "Prefix cookie names with $RELEASE_NAME"
-        sed -i 's|RELEASE_NAME|'$RELEASE_NAME'|g' /config/httpSession.xml
+        sed -i "s|RELEASE_NAME|$RELEASE_NAME|g" /config/httpSession.xml
 else
   echo "Prefix cookie names with $HOSTNAME"
-        sed -i 's|RELEASE_NAME|'$HOSTNAME'|g' /config/httpSession.xml
+        sed -i "s|RELEASE_NAME|$HOSTNAME|g" /config/httpSession.xml
 fi
 
-$SCRIPT/enableMetering.sh
+"$SCRIPT"/enableMetering.sh
 
-$SCRIPT/configureDatabase.sh h2
+"$SCRIPT"/configureDatabase.sh h2
 
-$SCRIPT/updateDatasource.sh
+"$SCRIPT"/updateDatasource.sh
 
-$SCRIPT/updatePersistenceLocale.sh
+"$SCRIPT"/updatePersistenceLocale.sh
 
-$SCRIPT/configureTlsSecurity.sh
+"$SCRIPT"/configureTlsSecurity.sh
 
-$SCRIPT/addDCApplications.sh /config/decisioncenter_application.xml
+"$SCRIPT"/addDCApplications.sh /config/decisioncenter_application.xml
 
-$SCRIPT/setTimeZone.sh
+"$SCRIPT"/setTimeZone.sh
 
-. $SCRIPT/setUTF8Locale.sh
+. "$SCRIPT"/setUTF8Locale.sh
 
 /opt/ibm/wlp/bin/server run defaultServer
