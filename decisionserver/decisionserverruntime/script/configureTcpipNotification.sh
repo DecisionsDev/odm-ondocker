4e9db585f5b8a0ad115c071b74610592376c44d2
Julie Garrone <julie.garrone@fr.ibm.com>
2020-07-07 15:30:49 +0200

    Fix decisionserverruntime scripts

--- decisionserver/decisionserverruntime/script/configureTcpipNotification.sh
+++ decisionserver/decisionserverruntime/script/configureTcpipNotification.sh
@@ -12,4 +12,4 @@ fi
 
 echo "Enable tcpip notification between Decision Server Runtime XU and Decision Server Console $tcpipHost on port $tcpipPort"
 
-sed -i 's/protocol=jmx/protocol=tcpip,tcpip.port='$tcpipPort',tcpip.host='$tcpipHost',tcpip.retryInterval=1000/' $APPS/DecisionService.war/WEB-INF/classes/ra.xml;
+sed -i "s/protocol=jmx/protocol=tcpip,tcpip.port=$tcpipPort,tcpip.host=$tcpipHost,tcpip.retryInterval=1000/" "$APPS"/DecisionService.war/WEB-INF/classes/ra.xml;
