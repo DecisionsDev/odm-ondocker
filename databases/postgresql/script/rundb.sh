f35d1f0579621919e1d160d8da4b058949952157
laurent grateau <laurent.grateau@fr.ibm.com>
2020-08-25 16:23:22 +0200

    Move to postgresql 12

--- databases/postgresql/script/rundb.sh
+++ databases/postgresql/script/rundb.sh
@@ -2,12 +2,27 @@
 
 set -e
 
-if [ ! -f /var/lib/postgresql/initialized.flag ] ; then
-	if [ "$SAMPLE" = "true" ] ; then
-		mkdir -p "$PGDATA"
-		cp -R /upload/* "$PGDATA"
+
+[ -z ${PGDATA} ] && PGDATA=/var/lib/pgsql/data/userdata
+
+# Depending on the postgresql implementation the directory to write is not the same.
+if [ -d /var/lib/pgsql ]; then 
+	INITFLAG=/var/lib/pgsql/initialized.flag
+else
+	INITFLAG=/var/lib/postgresql
+fi
+
+if [ ! -f $INITFLAG ] ; then
+	if [ "${SAMPLE}" = "true" ] ; then
+		mkdir -p ${PGDATA}
+		cp -R /upload/* ${PGDATA}/
 	fi;
-	touch /var/lib/postgresql/initialized.flag
+	touch $INITFLAG
 fi;
 
-exec "docker-entrypoint.sh" "$@"
+if type "run-postgresql" >& /dev/null ; then  
+	exec "run-postgresql"  
+else 
+	exec "docker-entrypoint.sh" "$@"
+fi
+
