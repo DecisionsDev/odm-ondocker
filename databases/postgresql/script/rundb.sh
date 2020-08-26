f16559a459688a508f10c6ede2a76921f17ea275
laurent grateau <laurent.grateau@fr.ibm.com>
2020-08-26 11:51:22 +0200

    Instead of copying the db use pg_restore this will allow to ensure compatibility in the future.

--- databases/postgresql/script/rundb.sh
+++ databases/postgresql/script/rundb.sh
@@ -2,24 +2,6 @@
 
 set -e
 
-
-[ -z ${PGDATA} ] && PGDATA=/var/lib/pgsql/data/userdata
-
-# Depending on the postgresql implementation the directory to write is not the same.
-if [ -d /var/lib/pgsql ]; then 
-	INITFLAG=/var/lib/pgsql/initialized.flag
-else
-	INITFLAG=/var/lib/postgresql
-fi
-
-if [ ! -f $INITFLAG ] ; then
-	if [ "${SAMPLE}" = "true" ] ; then
-		mkdir -p ${PGDATA}
-		cp -R /upload/* ${PGDATA}/
-	fi;
-	touch $INITFLAG
-fi;
-
 if type "run-postgresql" >& /dev/null ; then  
 	exec "run-postgresql"  
 else 
