0e10e6ac6eb9daf3b23a91c0293118ec99a2d39c
mathias-mouly <mathias.mouly@fr.ibm.com>
2020-10-23 13:44:19 +0200

    remove en_US.UTF-8 locale usage

--- decisioncenter/script/setUTF8Locale.sh
+++ decisioncenter/script/setUTF8Locale.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-if [ -f "/etc/redhat-release" ]
-then
-        echo "Set UTF-8 locale to en_US.UTF-8"
-        export LANG=en_US.UTF-8
-        export LC_ALL=en_US.UTF-8
-else
-        echo "Set locale to C.UTF-8"
-        export LANG=C.UTF-8
-        export LC_ALL=C.UTF-8
-fi
+echo "Set locale to C.UTF-8"
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+
 locale
