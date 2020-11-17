0086d24b22efed29b4d32bbf0ff5694f70562c6d
mathias-mouly <mathias.mouly@fr.ibm.com>
2020-11-17 17:34:10 +0100

    first IAM config

---
+++ iam/token-password.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# https://www.ibm.com/support/knowledgecenter/SSHKN6/iam/3.x.x/apis/oidc_auth_apis.html#get2
+
+token_url=https://cp-console.apps.ocp461dba.cp.fyre.ibm.com/idprovider/v1/auth/identitytoken
+
+client_id=iamclient4odm
+client_secret=iamsecretodm
+username=mathias.mouly@fr.ibm.com
+password=XXXX
+
+scope=openid
+
+curl -v -k -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" \
+     -d "scope=${scope}&grant_type=password&client_id=${client_id}&client_secret=${client_secret}&username=${username}&password=${password}" \
+     ${token_url}
+
+echo ""
