b4e37626fb16e167ee49e0d007d3258828b35040
mathias-mouly <mathias.mouly@fr.ibm.com>
2020-12-31 17:04:28 +0100

    #https://github.ibm.com/dba/icp4a-odm/issues/407

--- common/script/configureTlsSecurity.sh
+++ common/script/configureTlsSecurity.sh
@@ -48,25 +48,25 @@ else
 fi
 # End - Configuration for the TLS security
 
-if [ -f "/config/security/ldap.jks" ]
+if [ -f "/config/ldap/ldap.jks" ]
 then
         if [ -n "$LDAP_TRUSTSTORE_PASSWORD" ]
         then
-                echo "import /config/security/ldap.jks in trustore using provided LDAP truststore password"
+                echo "import /config/ldap/ldap.jks in trustore using provided LDAP truststore password"
         else
-                echo "import /config/security/ldap.jks in trustore using default LDAP truststore password"
+                echo "import /config/ldap/ldap.jks in trustore using default LDAP truststore password"
                 LDAP_TRUSTSTORE_PASSWORD=changeit
         fi
 
         i=0
-        mapfile -t trust_list < <(keytool -list -v -keystore /config/security/ldap.jks -storepass $LDAP_TRUSTSTORE_PASSWORD | grep "Alias name" | awk 'NF>1{print $NF}')
+        mapfile -t trust_list < <(keytool -list -v -keystore /config/ldap/ldap.jks -storepass $LDAP_TRUSTSTORE_PASSWORD | grep "Alias name" | awk 'NF>1{print $NF}')
         for trust_file in "${trust_list[@]}"
         do
-        keytool -changealias -alias ${trust_file} -destalias "LDAP_ALIAS_FOR_ODM_"$i -keystore /config/security/ldap.jks -storepass $LDAP_TRUSTSTORE_PASSWORD
+        keytool -changealias -alias ${trust_file} -destalias "LDAP_ALIAS_FOR_ODM_"$i -keystore /config/ldap/ldap.jks -storepass $LDAP_TRUSTSTORE_PASSWORD
         ((i=i+1))
         done
-        keytool -importkeystore -srckeystore /config/security/ldap.jks -destkeystore /config/security/truststore.jks -srcstorepass $LDAP_TRUSTSTORE_PASSWORD -deststorepass $DEFAULT_TRUSTSTORE_PASSWORD
+        keytool -importkeystore -srckeystore /config/ldap/ldap.jks -destkeystore /config/security/truststore.jks -srcstorepass $LDAP_TRUSTSTORE_PASSWORD -deststorepass $DEFAULT_TRUSTSTORE_PASSWORD
 
 else
-        echo "no /config/security/ldap.jks file"
+        echo "no /config/ldap/ldap.jks file"
 fi
