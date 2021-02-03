8b853d95a47c92818fd8c35143dd7e5792f5a928
mathias-mouly <mathias.mouly@fr.ibm.com>
2021-02-03 13:58:23 +0100

    refactor metering and add METERING_SEND_PERIOD variable

--- decisionserver/decisionrunner/script/enableDRMetering.sh
+++ decisionserver/decisionrunner/script/enableDRMetering.sh
@@ -6,23 +6,5 @@ then
 	cd  /config/apps/DecisionRunner.war/WEB-INF/classes;
 	sed -i 's/{pluginClass=DVS}/{pluginClass=Metering,enable=true},{pluginClass=DVS}/g' ra.xml
 
-        if [ -n "$METERING_SERVER_URL" ]
-        then
-                if [ -s "/config/pluginconfig/plugin-configuration.properties" ]
-                then
-                        echo "Configure metering using /config/pluginconfig/plugin-configuration.properties provided config"
-                else
-                        echo "Configure metering using /config/metering-template.properties template"
-                	sed -i 's|METERING_SERVER_URL|'$METERING_SERVER_URL'|g' /config/metering-template.properties
-                	if [ -n "$RELEASE_NAME" ]
-                	then
-                        	echo "Set METERING_INSTANCE_ID with $RELEASE_NAME"
-                        	sed -i 's|METERING_INSTANCE_ID|'$RELEASE_NAME'|g' /config/metering-template.properties
-                	else
-                        	echo "Set METERING_INSTANCE_ID with $HOSTNAME"
-                        	sed -i 's|METERING_INSTANCE_ID|'$HOSTNAME'|g' /config/metering-template.properties
-                	fi
-                	mkdir /config/pluginconfig
-                	cp /config/metering-template.properties /config/pluginconfig/plugin-configuration.properties
-        fi
+	$SCRIPT/configureMetering.sh
 fi
