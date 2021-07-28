c9bb3277021e2834f40ecc6585c9301935aeefd3
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2021-07-28 11:25:44 +0200

    Move to github action (#279)
    * pb with rm swidtag on OKD
    
    * Move to new release.
    
    * Renamed workflow
    
    * no message
    
    * First try
    
    * Get docker-compose
    
    * Env variables are not in a list, it's a YAML dict
    
    * Try sudo
    
    * Try the full chain
    
    * Don't know what the (failing) egrep is about
    
    * Try to download ODM dist from right place
    
    * Debug
    
    * Try to get right value from secret
    
    * Try sth else
    
    * 8.10.5.1 seems to be unavailable
    
    * Debug
    
    * Removed debug
    
    * Update the VM
    
    * Try to get meaningful error messages
    
    * Try another way to build
    
    Co-authored-by: mathias-mouly <mathias.mouly@fr.ibm.com>
    Co-authored-by: Pierre-Yves Lochou <pylochou@fr.ibm.com>

--- build.sh
+++ build.sh
@@ -3,21 +3,17 @@ set -e
 echo "start building odm-ondocker..."
 echo "current build directory:"
 pwd
-cd ../
+cd ..
 
-# if [ ! -f $HOME/.cache/$ODM_FILE_NAME ]; then
-    echo "ODM distribution: Starting download..."
-    ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
-    curl  $ODM_ZIP_URL -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PWD}" -o $ODM_FILE_NAME
-    mv $ODM_FILE_NAME $HOME/.cache/
-    echo "ODM distribution: download finished..."
-#else
-#    echo "ODM distribution: Loading from cache..."
-#    echo "ODM distribution: Loading finished..."
-#fi
+mkdir ${HOME}/.cache
+
+echo "ODM distribution: Starting download..."
+ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
+curl ${ODM_ZIP_URL} -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}" -o ${HOME}/.cache/${ODM_FILE_NAME}
+echo "ODM distribution: download finished..."
 
 echo "unzip odm distribution..."
-unzip -q $HOME/.cache/$ODM_FILE_NAME
+unzip -q ${HOME}/.cache/${ODM_FILE_NAME}
 
 echo "copy odm-ondocker into ODM distribution..."
 cp -R odm-ondocker install
@@ -27,7 +23,7 @@ cp resources/.dockerignore ../
 # Optimizing the build to download webprofile package.
 source .env
 echo "Using this properties from .env file."
-cat .env 
+cat .env
 docker run --user 'root' -v $PWD/wlp:/opt/wlp  $FROMLIBERTY  /bin/sh -c "mkdir -p /opt/wlp ;\
  installUtility download openidconnectclient-1.0 collectiveMember-1.0 sessionCache-1.0 ldapRegistry-3.0 localConnector-1.0 \
   microProfile-1.0 microProfile-1.2 microProfile-1.3 microProfile-1.4 monitor-1.0 restConnector-1.0 \
@@ -36,13 +32,13 @@ docker run --user 'root' -v $PWD/wlp:/opt/wlp  $FROMLIBERTY  /bin/sh -c "mkdir -
 
 
 echo "build ODM standard docker images..."
-docker-compose build
+DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml build
 
 echo "build ODM standalone docker image..."
-docker-compose -f odm-standalone.yml build
+DOCKER_BUILDKIT=1 docker-compose -f odm-standalone.yml build
 
 # echo "build ODM standalone tomcat8 docker image..."
 # docker-compose -f odm-standalone-tomcat.yml build
 
 echo "build ODM cluster docker images..."
-docker-compose -f odm-cluster.yml build
+DOCKER_BUILDKIT=1 docker-compose -f odm-cluster.yml build
