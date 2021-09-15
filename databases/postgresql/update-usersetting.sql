dafc622b42faa3d11e5ffe4db3ed0eaede5a5076
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2021-09-15 13:59:49 +0200

    Implement support of context root in case of db sample.

---
+++ databases/postgresql/update-usersetting.sql
@@ -0,0 +1,4 @@
+update usersetting set value = 'http://localhost:9060/ODM_CONTEXT_ROOT/decisionmodel' where ilrkey = 'decisionmodel.server.url';
+update usersetting set value = 'http://localhost:9060/ODM_CONTEXT_ROOT/decisioncenter-api' where ilrkey = 'decisioncenter-api.server.url';
+
+
