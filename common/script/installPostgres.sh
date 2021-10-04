a85e1e6820563377e74c922cd958bc4842800eb7
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2021-10-04 14:53:43 +0200

    Fix postgreql download driver

--- common/script/installPostgres.sh
+++ common/script/installPostgres.sh
@@ -3,5 +3,5 @@
 # Install the driver for PostgreSQL
 echo "Install the driver for postgreSQL"
 cd /tmp
-curl -O -s https://jdbc.postgresql.org/download/postgresql-42.2.18.jar
+curl -O -s -k https://jdbc.postgresql.org/download/postgresql-42.2.18.jar
 mv postgres* /config/resources
