34ad36a20588bab6dd3e1d022d67921e16d3c5f8
mathias-mouly <mathias.mouly@fr.ibm.com>
2021-10-07 17:22:10 +0200

    adapt server update in demo mode for Zen

--- decisioncenter/script/updateDemoServers.sh
+++ decisioncenter/script/updateDemoServers.sh
@@ -51,9 +51,15 @@ else
 DR_URL=$PROTOCOL"://"$DR_HOST":"$DR_PORT"/DecisionRunner"
 fi
 
+if [ -n "$OPENID_CONFIG" ]
+then
+
+OPENID_PROVIDER=$(grep OPENID_PROVIDER /config/authOidc/openIdParameters.properties | sed "s/OPENID_PROVIDER=//g")
+echo "will update Decision Service Execution with $DSE_URL in OAUTH mode with provider $OPENID_PROVIDER"
+
 retry=0
-while [[ ("$(curl -X POST "http://localhost:9060/decisioncenter-api/v1/servers/d8cb5830-14aa-45e0-89e2-8837f4d91021" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DSE_URL\"}" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200) && ( $retry -lt 10) ]];do 
-	echo "try updating Decision Service Execution with $DSE_URL"
+while [[ ("$(curl -X POST "http://localhost:9060/decisioncenter-api/v1/servers/d8cb5830-14aa-45e0-89e2-8837f4d91021" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DSE_URL\",\"authenticationKind\": \"OAUTH\",\"authenticationProvider\": \"$OPENID_PROVIDER\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200) && ( $retry -lt 10) ]];do 
+	echo "retry updating Decision Service Execution with $DSE_URL in OAUTH mode with provider $OPENID_PROVIDER"
 	sleep 5
 	retry=$((retry + 1))
 done
@@ -63,10 +69,10 @@ if [[ $retry == 10 ]];then
 else
 	echo "successfully updated Decision Service Execution with $DSE_URL"
 fi
-
+echo "will update Test and Simulation Execution with $DR_URL in OAUTH mode with provider $OPENID_PROVIDER"
 retry=0
-while [[ ("$(curl -X POST "http://localhost:9060/decisioncenter-api/v1/servers/a677f1c1-8633-42ff-8e4e-994fb52b3384" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DR_URL\"}" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200)  && ( $retry -lt 10) ]];do
-        echo "try updating Test and Simulation Execution with $DR_URL"
+while [[ ("$(curl -X POST "http://localhost:9060/decisioncenter-api/v1/servers/a677f1c1-8633-42ff-8e4e-994fb52b3384" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DR_URL\",\"authenticationKind\": \"OAUTH\",\"authenticationProvider\": \"$OPENID_PROVIDER\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200)  && ( $retry -lt 10) ]];do
+        echo "retry updating Test and Simulation Execution with $DR_URL in OAUTH mode with provider $OPENID_PROVIDER"
         sleep 5
 	retry=$((retry + 1))
 done
@@ -75,4 +81,35 @@ if [[ $retry == 10 ]];then
         echo "failed to update Test and Simulation Execution with $DR_URL"
 else
 	echo "successfully updated Test and Simulation Execution with $DR_URL"
+fi
+
+else
+echo "will update Decision Service Execution with $DSE_URL"
+retry=0
+while [[ ("$(curl -X POST "http://localhost:9060/decisioncenter-api/v1/servers/d8cb5830-14aa-45e0-89e2-8837f4d91021" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DSE_URL\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200) && ( $retry -lt 10) ]];do
+        echo "retry updating Decision Service Execution with $DSE_URL"
+        sleep 5
+        retry=$((retry + 1))
+done
+
+if [[ $retry == 10 ]];then
+        echo "failed to update Decision Service Execution with $DSE_URL"
+else
+        echo "successfully updated Decision Service Execution with $DSE_URL"
+fi
+
+echo "will update Test and Simulation Execution with $DR_URL"
+retry=0
+while [[ ("$(curl -X POST "http://localhost:9060/decisioncenter-api/v1/servers/a677f1c1-8633-42ff-8e4e-994fb52b3384" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DR_URL\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200)  && ( $retry -lt 10) ]];do
+        echo "try updating Test and Simulation Execution with $DR_URL"
+        sleep 5
+        retry=$((retry + 1))
+done
+
+if [[ $retry == 10 ]];then
+        echo "failed to update Test and Simulation Execution with $DR_URL"
+else
+        echo "successfully updated Test and Simulation Execution with $DR_URL"
+fi
+
 fi
