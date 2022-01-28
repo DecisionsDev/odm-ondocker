c53190172f8da8ee772c1e27ccd7e35c97377572
mathias-mouly <mathias.mouly@fr.ibm.com>
2022-01-28 11:50:04 +0100

    update server IDs

--- decisioncenter/script/updateDemoServers.sh
+++ decisioncenter/script/updateDemoServers.sh
@@ -55,7 +55,7 @@ OPENID_PROVIDER=$(grep OPENID_PROVIDER /config/authOidc/openIdParameters.propert
 echo "will update Decision Service Execution with $DSE_URL in OAUTH mode with provider $OPENID_PROVIDER"
 
 retry=0
-while [[ ("$(curl -X POST "$CURL_URL/decisioncenter-api/v1/servers/d8cb5830-14aa-45e0-89e2-8837f4d91021" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DSE_URL\",\"authenticationKind\": \"OAUTH\",\"authenticationProvider\": \"$OPENID_PROVIDER\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200) && ( $retry -lt 10) ]];do 
+while [[ ("$(curl -X POST "$CURL_URL/decisioncenter-api/v1/servers/62293377-13e9-457f-b756-7710357ca1ab" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DSE_URL\",\"authenticationKind\": \"OAUTH\",\"authenticationProvider\": \"$OPENID_PROVIDER\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200) && ( $retry -lt 10) ]];do 
 	echo "retry updating Decision Service Execution with $DSE_URL in OAUTH mode with provider $OPENID_PROVIDER"
 	sleep 5
 	retry=$((retry + 1))
@@ -68,7 +68,7 @@ else
 fi
 echo "will update Test and Simulation Execution with $DR_URL in OAUTH mode with provider $OPENID_PROVIDER"
 retry=0
-while [[ ("$(curl -X POST "$CURL_URL/decisioncenter-api/v1/servers/a677f1c1-8633-42ff-8e4e-994fb52b3384" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DR_URL\",\"authenticationKind\": \"OAUTH\",\"authenticationProvider\": \"$OPENID_PROVIDER\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200)  && ( $retry -lt 10) ]];do
+while [[ ("$(curl -X POST "$CURL_URL/decisioncenter-api/v1/servers/f4f01813-1ec1-4c89-b773-3d9e2afafab1" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DR_URL\",\"authenticationKind\": \"OAUTH\",\"authenticationProvider\": \"$OPENID_PROVIDER\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200)  && ( $retry -lt 10) ]];do
         echo "retry updating Test and Simulation Execution with $DR_URL in OAUTH mode with provider $OPENID_PROVIDER"
         sleep 5
 	retry=$((retry + 1))
@@ -83,7 +83,7 @@ fi
 else
 echo "will update Decision Service Execution with $DSE_URL"
 retry=0
-while [[ ("$(curl -X POST "$CURL_URL/decisioncenter-api/v1/servers/d8cb5830-14aa-45e0-89e2-8837f4d91021" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DSE_URL\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200) && ( $retry -lt 10) ]];do
+while [[ ("$(curl -X POST "$CURL_URL/decisioncenter-api/v1/servers/62293377-13e9-457f-b756-7710357ca1ab" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DSE_URL\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200) && ( $retry -lt 10) ]];do
         echo "retry updating Decision Service Execution with $DSE_URL"
         sleep 5
         retry=$((retry + 1))
@@ -97,7 +97,7 @@ fi
 
 echo "will update Test and Simulation Execution with $DR_URL"
 retry=0
-while [[ ("$(curl -X POST "$CURL_URL/decisioncenter-api/v1/servers/a677f1c1-8633-42ff-8e4e-994fb52b3384" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DR_URL\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200)  && ( $retry -lt 10) ]];do
+while [[ ("$(curl -X POST "$CURL_URL/decisioncenter-api/v1/servers/f4f01813-1ec1-4c89-b773-3d9e2afafab1" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"url\": \"$DR_URL\" }" -H "authorization: Basic b2RtQWRtaW46b2RtQWRtaW4=" -w "%{http_code}" -s -o /dev/null)" != 200)  && ( $retry -lt 10) ]];do
         echo "try updating Test and Simulation Execution with $DR_URL"
         sleep 5
         retry=$((retry + 1))
