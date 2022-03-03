3ef54b4c1d793b79b7908fdd4b9730464d8a9144
mathias-mouly <mathias.mouly@fr.ibm.com>
2022-03-03 09:04:49 +0100

    keycloak openID configuration

--- keycloak/token-clientcredentials.sh
+++ keycloak/token-clientcredentials.sh
@@ -5,7 +5,7 @@ source env-keycloak-trial.sh
 scope=openid
 
 curl -k -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" \
-     -d "scope=${scope}&grant_type=password&client_id=${client_id}&client_secret=${client_secret}&username=${username}&password=${password}" \
+     -d "scope=${scope}&grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}" \
      ${token_url} | jq
 
 echo ""
