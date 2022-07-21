51161ab20a3e448c561f5ae8584ee285eb1d021d
fredibm <35365560+fredibm@users.noreply.github.com>
2022-07-21 12:56:58 +0200

    keep only decision services in samples

--- contrib/populate-sample-db/populate.sh
+++ contrib/populate-sample-db/populate.sh
@@ -44,22 +44,6 @@ if [[ "${decisionServiceId}" == "null" ]]; then
   decisionServiceId=4ea8ed3f-98a0-4b25-853c-6cc857215ae8
 fi
 
-echo -n "$(date) - ### Upload Shipment Pricing to DC:  "
-curl_result=$(curl --silent --insecure --request POST "${DC_URL}/decisioncenter-api/v1/decisionservices/import" --header "accept: */*" --header "Content-Type: multipart/form-data" --form "file=@$(dirname "$0")/Shipment_Pricing_main.zip;type=application/zip" --user ${DC_USER}:${DC_USER})
-if [[ $? != 0 ]]; then
-  echo "Could not connect to ${DC_URL};  please check that DC is up and running."
-  exit 1
-fi
-status=$(echo ${curl_result} | jq -r '.status')
-if [[ "${status}" == "null" ]]; then
-  echo "COMPLETED"
-else
-  echo ${status}
-fi
-if [[ "${status}" == "BAD_REQUEST" ]]; then
-  echo ${curl_result} | jq -r '.reason'
-fi
-
 echo -n "$(date) - ### Get elements Ids from DC:  "
 elements=$(curl --silent --insecure "${DC_URL}/decisioncenter-api/v1/decisionservices/${decisionServiceId}/deployments" --user ${DC_USER}:${DC_USER})
 if [[ $? != 0 ]]; then
