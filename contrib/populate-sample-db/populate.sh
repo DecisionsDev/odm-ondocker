087eb3cb7febf794cac9da061cc93af67e15cd2d
Mathias Mouly <mathias.mouly@fr.ibm.com>
2022-08-03 15:37:44 +0200

    Update populate.sh

--- contrib/populate-sample-db/populate.sh
+++ contrib/populate-sample-db/populate.sh
@@ -23,6 +23,7 @@ done
 
 type jq >& /dev/null || (echo "jq must be installed!" && exit 1)
 
+# Get Decision Service https://github.com/DecisionsDev/odm-for-dev-getting-started/blob/master/Loan%20Validation%20Service.zip
 echo -n "$(date) - ### Upload Loan Validation Service to DC:  "
 curl_result=$(curl --silent --insecure --request POST "${DC_URL}/decisioncenter-api/v1/decisionservices/import" --header "accept: */*" --header "Content-Type: multipart/form-data" --form "file=@$(dirname "$0")/Loan_Validation_Service_main.zip;type=application/zip" --user ${DC_USER}:${DC_USER})
 if [[ $? != 0 ]]; then
@@ -44,6 +45,7 @@ if [[ "${decisionServiceId}" == "null" ]]; then
   decisionServiceId=4ea8ed3f-98a0-4b25-853c-6cc857215ae8
 fi
 
+# Get Decision Service https://github.com/DecisionsDev/odm-for-dev-getting-started/blob/master/Miniloan%20Service.zip
 echo -n "$(date) - ### Upload Miniloan Service to DC:  "
 curl_result=$(curl --silent --insecure --request POST "${DC_URL}/decisioncenter-api/v1/decisionservices/import" --header "accept: */*" --header "Content-Type: multipart/form-data" --form "file=@$(dirname "$0")/Miniloan_Service_main.zip;type=application/zip" --user ${DC_USER}:${DC_USER})
 if [[ $? != 0 ]]; then
