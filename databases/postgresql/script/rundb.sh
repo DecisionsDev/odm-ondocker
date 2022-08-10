ddcafe10fdff5e885a4c4f8c826d47d1c557b617
Julie Garrone <julie.garrone@fr.ibm.com>
2022-08-10 11:22:48 +0200

    Fix usage of postgres credentials files

--- databases/postgresql/script/rundb.sh
+++ databases/postgresql/script/rundb.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-[ -f /run/secrets/postgres-config/db-user ] && export POSTGRES_USER=$(cat /run/secrets/postgres-config/db-user)
-[ -f /run/secrets/postgres-config/db-password ] && export POSTGRES_PASSWORD=$(cat /run/secrets/postgres-config/db-password)
-unset POSTGRES_USER_FILE
-unset POSTGRES_PASSWORD_FILE
+[ -f /run/secrets/postgres-config/db-user ] && export POSTGRES_USER=$(cat /run/secrets/postgres-config/db-user) && export POSTGRESQL_USER=$(cat /run/secrets/postgres-config/db-user)
+[ -f /run/secrets/postgres-config/db-password ] && export POSTGRES_PASSWORD=$(cat /run/secrets/postgres-config/db-password) && export POSTGRESQL_PASSWORD=$(cat /run/secrets/postgres-config/db-password)
 
 if type "run-postgresql" >& /dev/null ; then
 	exec "run-postgresql"
