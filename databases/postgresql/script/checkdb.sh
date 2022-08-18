8045733e786f455badbbea2a4a9743f9dc0a6249
Julie Garrone <47252804+julie-garrone@users.noreply.github.com>
2022-08-18 07:19:48 +0200

    DBACLD-56118 - Add checkdb script (#314)
    Add checkdb.sh script to replace command line in init container

---
+++ databases/postgresql/script/checkdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ -n "$PGUSER_FILE" ] && export PGUSER=$(cat $PGUSER_FILE)
+[ -n "$PGPASSWORD_FILE" ] && export PGPASSWORD=$(cat $PGPASSWORD_FILE)
+
+CHECK_DB_SERVER=2
+
+until [ $CHECK_DB_SERVER -eq 0 ]
+do
+  echo "Test connection to $PGHOST on port 5432 state $CHECK_DB_SERVER"
+  CHECK_DB_SERVER=$(psql -q -h $PGHOST -d $PGDATABASE -c "select 1" -p 5432 >/dev/null;echo $?)
+  echo "Check $CHECK_DB_SERVER"
+  sleep 2
+done
