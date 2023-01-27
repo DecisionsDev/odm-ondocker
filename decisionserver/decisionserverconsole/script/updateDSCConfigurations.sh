4230b031eee4fb41eac5d9091ec76dd4af030e76
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2023-01-27 12:42:30 +0100

    Fix DBACLD-79445 duplicate context-root

--- decisionserver/decisionserverconsole/script/updateDSCConfigurations.sh
+++ decisionserver/decisionserverconsole/script/updateDSCConfigurations.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$DECISION_SERVICE_URL" ]; then
 	echo "Update DECISION_SERVICE_URL to $DECISION_SERVICE_URL in Decision Server Console."
-	sed -i 's|/DecisionService|'$DECISION_SERVICE_URL'|g' $APPS/res.war/WEB-INF/web.xml
+	sed -i ‘s|>/DecisionService|>’$DECISION_SERVICE_URL’|g’ $APPS/res.war/WEB-INF/web.xml
 fi
 
 if [ -f "/config/baiemitterconfig/plugin-configuration.properties" ]; then
