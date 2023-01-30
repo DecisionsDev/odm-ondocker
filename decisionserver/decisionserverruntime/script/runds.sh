9f3b133afe447ca641a8c43f0b0330ed603079dd
laurent grateau <laurent.grateau@fr.ibm.com>
2023-01-30 14:22:07 +0100

    Fix DBACLD-79445

--- decisionserver/decisionserverruntime/script/runds.sh
+++ decisionserver/decisionserverruntime/script/runds.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
 . $SCRIPT/initVariables.sh 9080 9443
-
-if [ -s "$SCRIPT/customStart.sh" ]
+if [ ! -f /config/initialized ]
 then
-        $SCRIPT/customStart.sh
-fi
+        if [ -s "$SCRIPT/customStart.sh" ]
+        then
+                $SCRIPT/customStart.sh
+        fi
 
-if [ -n "$USERS_PASSWORD" ]
-then
-  echo "Set password for defaut users"
-  sed -i 's|password=".*"|'password=\"$USERS_PASSWORD\"'|g' /config/auth/webSecurity.xml
-fi
+        if [ -n "$USERS_PASSWORD" ]
+        then
+        echo "Set password for defaut users"
+        sed -i 's|password=".*"|'password=\"$USERS_PASSWORD\"'|g' /config/auth/webSecurity.xml
+        fi
 
-$SCRIPT/enableMetering.sh
+        $SCRIPT/enableMetering.sh
 
-$SCRIPT/updateDSRConfigurations.sh
+        $SCRIPT/updateDSRConfigurations.sh
 
-$SCRIPT/configureTcpipNotification.sh
+        $SCRIPT/configureTcpipNotification.sh
 
-$SCRIPT/configureTlsSecurity.sh
+        $SCRIPT/configureTlsSecurity.sh
 
-$SCRIPT/configureDatabase.sh
+        $SCRIPT/configureDatabase.sh
 
-$SCRIPT/updateDatasource.sh
+        $SCRIPT/updateDatasource.sh
 
-$SCRIPT/configureSwidTag.sh
+        $SCRIPT/configureSwidTag.sh
 
-$SCRIPT/setTimeZone.sh
+        $SCRIPT/setTimeZone.sh
 
-if [ -s "$SCRIPT/customEnd.sh" ]
-then
-        $SCRIPT/customEnd.sh
+        if [ -s "$SCRIPT/customEnd.sh" ]
+        then
+                $SCRIPT/customEnd.sh
+        fi
+        touch /config/initialized
 fi
