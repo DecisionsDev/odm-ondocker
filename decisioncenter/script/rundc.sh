7088d8bfed88e248c57d409951bb86d914cf8d01
laurent grateau <laurent.grateau@fr.ibm.com>
2023-01-30 14:27:25 +0100

    Fix DBACLD-79445

--- decisioncenter/script/rundc.sh
+++ decisioncenter/script/rundc.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
 . $SCRIPT/initVariables.sh 9060 9453
-
-if [ -s "$SCRIPT/customStart.sh" ]
+if [ ! -f /config/initialized ]
 then
-	$SCRIPT/customStart.sh
+  if [ -s "$SCRIPT/customStart.sh" ]
+  then
+    $SCRIPT/customStart.sh
+  fi
+
+  $SCRIPT/addDCApplications.sh
+
+  $SCRIPT/updateDCConfigurations.sh
+
+  if [ -n "$USERS_PASSWORD" ]
+  then
+    echo "Set password for defaut users"
+    sed -i 's|password=".*"|'password=\"$USERS_PASSWORD\"'|g' /config/auth/webSecurity.xml
+    sed -i 's|"loginPassword": ".*"|'\"loginPassword\":\"$USERS_PASSWORD\"'|g' /config/server-configurations.json
+  fi
+
+  $SCRIPT/updatePersistenceLocale.sh
+
+  $SCRIPT/configureTlsSecurity.sh
+
+  $SCRIPT/configureDatabase.sh
+
+  $SCRIPT/updateDatasource.sh
+
+  $SCRIPT/configureSwidTag.sh
+
+  $SCRIPT/setTimeZone.sh
+
+  . $SCRIPT/setUTF8Locale.sh
+
+  $SCRIPT/generateRDFiles.sh
+
+  if [ -s "$SCRIPT/customEnd.sh" ]
+  then
+          $SCRIPT/customEnd.sh
+  fi
+  touch /config/initialized
 fi
-
-$SCRIPT/addDCApplications.sh
-
-$SCRIPT/updateDCConfigurations.sh
-
-if [ -n "$USERS_PASSWORD" ]
-then
-  echo "Set password for defaut users"
-  sed -i 's|password=".*"|'password=\"$USERS_PASSWORD\"'|g' /config/auth/webSecurity.xml
-  sed -i 's|"loginPassword": ".*"|'\"loginPassword\":\"$USERS_PASSWORD\"'|g' /config/server-configurations.json
-fi
-
-$SCRIPT/updatePersistenceLocale.sh
-
-$SCRIPT/configureTlsSecurity.sh
-
-$SCRIPT/configureDatabase.sh
-
-$SCRIPT/updateDatasource.sh
-
-$SCRIPT/configureSwidTag.sh
-
-$SCRIPT/setTimeZone.sh
-
-. $SCRIPT/setUTF8Locale.sh
-
-$SCRIPT/generateRDFiles.sh
-
-if [ -s "$SCRIPT/customEnd.sh" ]
-then
-        $SCRIPT/customEnd.sh
-fi
-
 if [ -n "$DEMO" ]
 then
 	$SCRIPT/updateDemoServers.sh &
