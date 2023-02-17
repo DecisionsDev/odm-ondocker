7d4f2a3e5192b07d6520084f157632f9e1473321
laurent grateau <laurent.grateau@fr.ibm.com>
2023-02-17 10:44:30 +0100

    Miss to copy version file.

--- common/script/generateVersionFile.sh
+++ common/script/generateVersionFile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSIONFILE="/opt/ibm/version.txt"
 echo "IBM Operational Decision Manager (CloudPak) : "$CP4BAVERSION > $VERSIONFILE
-echo "IBM Operational Decision Manager (ODM on Certified Kubernetes) :"$ODMVERSION >> $VERSIONFILE
+echo "IBM Operational Decision Manager (ODM on Certified Kubernetes) : "$ODMVERSION >> $VERSIONFILE
