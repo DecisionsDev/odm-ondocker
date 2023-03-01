0b15057bbeb9538f8a6c006c482db4a3795e4d67
laurent grateau <laurent.grateau@fr.ibm.com>
2023-03-01 18:09:47 +0100

    Work on DBACLD-83524

--- common/script/generateVersionFile.sh
+++ common/script/generateVersionFile.sh
@@ -2,3 +2,7 @@
 VERSIONFILE="/opt/ibm/version.txt"
 echo "IBM Operational Decision Manager (CloudPak) : "$CP4BAVERSION > $VERSIONFILE
 echo "IBM Operational Decision Manager (ODM on Certified Kubernetes) : "$ODMVERSION >> $VERSIONFILE
+LIBERTY_VERSION=$(/opt/ibm/wlp/bin/server version)
+echo "Liberty : "${LIBERTY_VERSION}  >> $VERSIONFILE
+JAVA_VERSION=$(java --version | head -2 | tail -1)
+echo "Java : "${JAVA_VERSION}  >> $VERSIONFILE
