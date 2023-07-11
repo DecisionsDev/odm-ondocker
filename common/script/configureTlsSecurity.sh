464dd1106788b6834730d01b45069891256dc176
mmouly <mathias.mouly@fr.ibm.com>
2023-07-11 11:10:14 +0200

    #DBACLD-97272 Import Private Certifiate

--- common/script/configureTlsSecurity.sh
+++ common/script/configureTlsSecurity.sh
@@ -131,6 +131,25 @@ if [ -d $CERTDIR ]; then
     echo "done"
 fi
 
+# This part allow to import a list of PEM certificate in the JVM
+ echo "Importing private certificates $dir"
+PRIVATE_CERTDIR="/config/security/private-cert-volume/"
+if [ -d $PRIVATE_CERTDIR ]; then
+    cd $PRIVATE_CERTDIR
+    for dir in *; do
+        echo "Importing private certificates $dir"
+        if [ -d $dir ]; then
+           if [ -f $dir/tls.key ]; then
+                openssl pkcs12 -export -inkey $dir/tls.key -in $dir/tls.crt -name $dir -out /config/security/$dir.p12 -passout pass:$DEFAULT_KEYSTORE_PASSWORD
+                keytool -J"-Xshareclasses:none" -importkeystore -srckeystore /config/security/$dir.p12 -srcstorepass $DEFAULT_KEYSTORE_PASSWORD -srcstoretype PKCS12 -destkeystore /config/security/keystore.jks -deststoretype JKS -deststorepass $DEFAULT_KEYSTORE_PASSWORD
+           else
+                echo "Couldn't find certificate $dir/tls.key skipping this certificate "
+           fi
+        fi
+    done
+    echo "done"
+fi
+
 if [ -n "$ENABLED_CIPHERS" ]
 then
 	echo "configure enabled ciphers with $ENABLED_CIPHERS"
