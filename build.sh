4245be2c481e6b0a8307be927c0bdee99542f804
Pierre-Yves Lochou <31895642+PYLochou@users.noreply.github.com>
2023-07-19 15:58:16 +0200

    Fix curl's problem with SSL certificate

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mkdir ${HOME}/.cache
 
 echo "ODM distribution: Starting download..."
 ODM_ZIP_URL=${ODM_URL}/${ODM_VERSION}/icp-docker-compose-build-images-${ODM_VERSION}.zip
-curl ${ODM_ZIP_URL} -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}" -o ${HOME}/.cache/${ODM_FILE_NAME}
+curl -k ${ODM_ZIP_URL} -u "${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}" -o ${HOME}/.cache/${ODM_FILE_NAME}
 echo "ODM distribution: download finished..."
 
 echo "unzip odm distribution..."
