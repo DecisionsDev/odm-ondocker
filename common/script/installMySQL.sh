21927f5d49e3c8f31ebc90d43e3977b3ba7f59aa
Pierre-Yves Lochou <pylochou@fr.ibm.com>
2023-12-19 09:48:06 +0100

    Updated JDBC driver release

--- common/script/installMySQL.sh
+++ common/script/installMySQL.sh
@@ -3,5 +3,5 @@
 # Install the driver for MySQL
 echo "Install the driver for MySQL"
 cd /tmp
-curl -O -s http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.22/mysql-connector-java-5.1.22.jar
+curl -O -s https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/8.2.0/mysql-connector-j-8.2.0.jar
 mv mysql* /config/resources
