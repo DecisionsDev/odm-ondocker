1b427cd2578f9bd74841124b50e8895c62db079f
Mathias Mouly <mathias.mouly@fr.ibm.com>
2024-02-21 17:12:47 +0100

    postgresql-42.7.2.jar

--- common/script/installPostgres.sh
+++ common/script/installPostgres.sh
@@ -3,6 +3,6 @@ set -e
 # Install the driver for PostgreSQL
 echo "Install the driver for postgreSQL"
 cd /tmp
-curl -O -k -s https://jdbc.postgresql.org/download/postgresql-42.7.1.jar --fail
+curl -O -k -s https://jdbc.postgresql.org/download/postgresql-42.7.2.jar --fail
 mv postgres* /config/resources
 set +e
