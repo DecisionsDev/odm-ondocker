410966dee2daf1d51bb296b46506e7f2e6265e99
Mathias Mouly <mathias.mouly@fr.ibm.com>
2024-02-21 10:09:52 +0100

    https://www.mend.io/vulnerability-database/CVE-2024-1597

--- common/script/installPostgres.sh
+++ common/script/installPostgres.sh
@@ -3,5 +3,5 @@
 # Install the driver for PostgreSQL
 echo "Install the driver for postgreSQL"
 cd /tmp
-curl -O -k -s https://jdbc.postgresql.org/download/postgresql-42.5.1.jar
+curl -O -k -s https://jdbc.postgresql.org/download/postgresql-42.7.2.jar
 mv postgres* /config/resources
