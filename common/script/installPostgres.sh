b588e8af1de50ea3541bb5343687874341fcc749
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2024-02-21 11:28:34 +0100

    Try to fail build if download failed

--- common/script/installPostgres.sh
+++ common/script/installPostgres.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+set -e
 # Install the driver for PostgreSQL
 echo "Install the driver for postgreSQL"
 cd /tmp
-curl -O -k -s https://jdbc.postgresql.org/download/postgresql-42.7.2.jar
+curl -O -k -s https://jdbc.postgresql.org/download/postgresql-42.7.2.jar --fail
 mv postgres* /config/resources
+set +e
