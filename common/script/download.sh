eed367ef357a0418c838e09cfbc7c1a0edf84d1d
mmouly <mathias.mouly@fr.ibm.com>
2024-03-20 12:32:40 +0100

    https://jsw.ibm.com/browse/DBACLD-123096

--- common/script/download.sh
+++ common/script/download.sh
@@ -8,30 +8,35 @@ then
            mkdir /config/download
         fi
 
+	cd /config/download
+
         # Download files from each urls
         IFS=','
         read -a download_urls <<< "$DOWNLOAD_URL"
         for url in "${download_urls[@]}";
         do
-          (cd /config/download && curl -O -k -s -L $url)
+          curl -O -k -s -L $url
         done
 
         # Unzip download if necessary
         if [ -f /config/download/*.zip ]; then
-                (cd /config/download && unzip -q *.zip)
+                unzip -q *.zip
+		rm *.zip
         fi
 
         # Untar download if necessary (.tar, .tar.gz, .tar.bz2, .tar.xz are supported)
         if [ -f /config/download/*.tar* ]; then
                 for arch in "/config/download"/*.tar*
                 do
-                  (cd /config/download && tar -xaf $arch)
+                  tar -xaf $arch
+		  rm $arch
                 done
         fi
         if [ -f /config/download/*.tgz* ]; then
                 for arch in "/config/download"/*.tgz*
                 do
-                  (cd /config/download && tar -xaf $arch)
+                  tar -xaf $arch
+		  rm $arch
                 done
         fi
 fi
