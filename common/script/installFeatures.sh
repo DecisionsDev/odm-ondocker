2cbbdc6d2a86181384fa26e33e19fb13dcd53fa4
mmouly <mathias.mouly@fr.ibm.com>
2024-03-21 16:17:19 +0100

    move PACKAGELIST

--- common/script/installFeatures.sh
+++ common/script/installFeatures.sh
@@ -3,9 +3,6 @@
 # Install the driver for Derby
 echo "Install the feature list for ODM on Liberty"
 ROOTFEATUREDIR=/opt/wlppackage
-PACKAGELIST="mpMetrics-3.0 openidconnectclient-1.0 collectiveMember-1.0 sessionCache-1.0 ldapRegistry-3.0 localConnector-1.0 \
-  mpOpenAPI-2.0 mpOpenTracing-2.0 microProfile-4.0 microProfile-1.0 microProfile-1.2 microProfile-1.3 microProfile-1.4 monitor-1.0 restConnector-1.0 \
-  requestTiming-1.0 restConnector-2.0 sessionDatabase-1.0 ssl-1.0 transportSecurity-1.0 webCache-1.0 webProfile-7.0"
 
 if [ ! -d $ROOTFEATUREDIR/features ]; then
   mkdir -p $ROOTFEATUREDIR
