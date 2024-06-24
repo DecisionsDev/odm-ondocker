6fbf9eea993b552533469f307bccebac73ef0cd5
mmouly <mathias.mouly@fr.ibm.com>
2024-06-24 16:55:27 +0200

    remove keycloak

--- keycloak/oktaclient-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source env-okta-trial.sh
-
-curl -X DELETE \
-     -H "Accept: application/json" \
-     -H "Content-Type:application/json" \
-     -H "Authorization: SSWS ${api_token}" \
-     "${registration_url}/${client_id}" | jq
