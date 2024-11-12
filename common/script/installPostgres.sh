3c0033bd1c2b1ba14f11872b76e75c4da7f87457
Pierre-Yves Lochou <31895642+PYLochou@users.noreply.github.com>
2024-11-12 11:51:54 +0100

    Fix CVE-2024-1597

--- common/script/installPostgres.sh
+++ common/script/installPostgres.sh
@@ -3,5 +3,5 @@
 # Install the driver for PostgreSQL
 echo "Install the driver for postgreSQL"
 cd /tmp
-curl -O -k -s https://jdbc.postgresql.org/download/postgresql-42.4.1.jar
+curl -O -k -s https://jdbc.postgresql.org/download/postgresql-42.4.4.jar
 mv postgres* /config/resources
