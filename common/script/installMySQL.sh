71e39e707c5363d6651eae6b5febe275c5d13ef8
Frederic Mercier <frederic.mercier@gmx.net>
2025-01-08 19:31:47 +0100

    Update MySQL driver to 9.1.0

--- common/script/installMySQL.sh
+++ common/script/installMySQL.sh
@@ -3,5 +3,5 @@
 # Install the driver for MySQL
 echo "Install the driver for MySQL"
 cd /tmp
-curl -O -k -s https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/8.2.0/mysql-connector-j-8.2.0.jar
+curl -O -k -s https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/9.1.0/mysql-connector-j-9.1.0.jar
 mv mysql* /config/resources
