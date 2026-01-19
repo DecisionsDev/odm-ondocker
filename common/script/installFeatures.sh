788b59f6ed9ea53b30f6522229882e7d4a51fc2a
mmouly <mathias.mouly@fr.ibm.com>
2026-01-19 17:26:03 +0100

    DBACLD-207153 Improve build stability with featureutility

--- common/script/installFeatures.sh
+++ common/script/installFeatures.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Install the driver for Derby
 echo "Install the feature list for ODM on Liberty"
-ROOTFEATUREDIR=/opt/wlppackage
-
-if [ ! -d $ROOTFEATUREDIR/features ]; then
-  mkdir -p $ROOTFEATUREDIR
-  echo "Downloading features list : $PACKAGELIST"
-  installUtility download  $PACKAGELIST --location=$ROOTFEATUREDIR;
-fi
-installUtility install $PACKAGELIST --from=$ROOTFEATUREDIR
+featureUtility installFeature $PACKAGELIST
