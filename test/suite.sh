e645eb53b4c904e79694f3adce70ec599a513b6b
Laurent GRATEAU <laurent.grateau@fr.ibm.com>
2026-01-27 09:58:19 +0100

    Improve build and test. Add health check + verify authentication (#389)
    * Improve build and test. Add health check + verify authentication
    
    * Try to fix
    
    * increase timeout
    
    * Fix DC URL Tests
    
    * container renamed
    
    * Fix ping installation
    
    * fix warning

--- test/suite.sh
+++ test/suite.sh
@@ -32,8 +32,8 @@ check_for_docker_url () {
     dockerurl=$2
     echo "Test $dockerurl availability in $dockerimg image."
 
-    docker exec -u 0:0 -ti $dockerimg bash -c " \
-        yum install -y iputils
+    docker exec -u 0:0 -i $dockerimg bash -c " \
+        microdnf -y install iputils
         ping -q -c5 $dockerurl > /dev/null && \
         if [ $? -eq 0 ] ; then \
             echo \"OK: $dockerimg\"; \
@@ -50,17 +50,17 @@ check_for_docker_url () {
 
 wait_for_url http://localhost:9070/DecisionRunner
 wait_for_url http://localhost:9090/DecisionService resExecutor resExecutor
-wait_for_url http://localhost:9060/decisioncenter
-wait_for_url http://localhost:9080/res
+wait_for_url http://localhost:9060/decisioncenter-api/v1/decisionservices rtsAdmin rtsAdmin
+wait_for_url http://localhost:9080/res/api/v1/ruleapps resAdmin resAdmin
 wait_for_url http://localhost:9080/DecisionService resExecutor resExecutor
 
-check_for_docker_url odm-ondocker_odm-decisionrunner_1        dbserver
-check_for_docker_url odm-ondocker_odm-decisionserverruntime_1 dbserver
-check_for_docker_url odm-ondocker_odm-decisionserverconsole_1 dbserver
-check_for_docker_url odm-ondocker_odm-decisioncenter_1        dbserver
-check_for_docker_url odm-ondocker_odm-decisioncenter_1        odm-decisionrunner
-check_for_docker_url odm-ondocker_odm-decisioncenter_1        odm-decisionserverconsole
-check_for_docker_url odm-ondocker_odm-decisionserverruntime_1 odm-decisionserverconsole
+check_for_docker_url odm-ondocker-odm-decisionrunner-1        dbserver
+check_for_docker_url odm-ondocker-odm-decisionserverruntime-1 dbserver
+check_for_docker_url odm-ondocker-odm-decisionserverconsole-1 dbserver
+check_for_docker_url odm-ondocker-odm-decisioncenter-1        dbserver
+check_for_docker_url odm-ondocker-odm-decisioncenter-1        odm-decisionrunner
+check_for_docker_url odm-ondocker-odm-decisioncenter-1        odm-decisionserverconsole
+check_for_docker_url odm-ondocker-odm-decisionserverruntime-1 odm-decisionserverconsole
 
 
 exit $ret
